5328bcc77a1f8c5fcbb1b50409b223509ac56399
Mayur Ghule <mayur.ghule494@gmail.com>
2023-03-26 23:57:29 +0530

    Added comments and echo scripts

---
+++ comments.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# scripts for comments we use in scripts
+
+# Example
+#
+# sudo apt install jenkins
+#
+:
+'
+name= 'mayur'
+read= age
+'
+echo "Enter your name:"
+name= 'nishant'
+echo "Enter your age:"
+read= age
---
+++ echo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "this is the echo script"
+
+echo "hello everyone welcome!!!"
